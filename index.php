b43729c15a971570cc88a39dea5eb124da19aa58
Eric Sensus <eric@sensussystems.com>
2017-06-12 11:07:35 +0300

    Initial commit with nav bar

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+    require ''
+?>
+<html>
+    <head>
+        <title>Home | Shareride Inc.</title>
+
+        <!--bootstrap css-->
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+
+        <!--custom css-->
+        <link href="assets/css/app.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Shareride Inc.</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Get a Ride</a></li>
+                            <li><a href="#">Give a Ride</a></li>
+                            <li><a href="#">Available Rides</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav
+        </div>
+
+        <!--jquery lib-->
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+        <!--bootstrap js-->
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
